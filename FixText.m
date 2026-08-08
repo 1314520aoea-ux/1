c669565acf40f8e1ea179dd8565fd02b924c8a56
1314520aoea-ux <1314520aoea@gmail.com>
2026-08-08 23:05:02 +0800

    Update FixText.m

--- FixText.m
+++ FixText.m
@@ -3,46 +3,79 @@
 #import <objc/runtime.h>
 
 static IMP gOriginalSetText = NULL;
+static IMP gOriginalSetAttributedText = NULL;
 
-// 拦截并替换 UILabel 文字
-static void CustomSetText(UILabel *self, SEL _cmd, NSString *text) {
-    if (text && [text isKindOfClass:[NSString class]]) {
-        // 匹配 🄷🅆🅅🄸🄿 或 HWVIP
-        if ([text containsString:@"\U0001F137\U0001F146\U0001F145\U0001F138\U0001F13F"] || [text containsString:@"HWVIP"]) {
-            
-            BOOL isDock = NO;
-            UIView *curr = self;
-            
-            // 向上遍历父视图，判断是否属于底栏/Tab 栏容器
-            while (curr) {
-                NSString *cls = NSStringFromClass([curr class]);
-                if ([cls containsString:@"Dock"] || 
-                    [cls containsString:@"Tab"] || 
-                    [cls containsString:@"Bar"] || 
-                    [cls containsString:@"Bottom"]) {
-                    isDock = YES;
-                    break;
-                }
-                curr = curr.superview;
-            }
-            
-            if (isDock) {
-                text = @"\u9996\u9875"; // 底部 Dock 栏显示 "首页"
-            } else {
-                text = @"Profile";    // 详情页等其他地方恢复为 "Profile"
-            }
+// 判断是否属于底部 TabBar 或 Dock 容器
+static BOOL IsInDockBar(UIView *view) {
+    UIView *curr = view;
+    while (curr) {
+        NSString *cls = NSStringFromClass([curr class]);
+        // 严格精确匹配底部导航栏类名
+        if ([cls containsString:@"TabBar"] || 
+            [cls containsString:@"Dock"] || 
+            [cls containsString:@"UITabBarButton"]) {
+            return YES;
+        }
+        curr = curr.superview;
+    }
+    return NO;
+}
+
+// 提取与替换逻辑
+static NSString *GetReplacementText(UILabel *label, NSString *originalText) {
+    if (!originalText || ![originalText isKindOfClass:[NSString class]]) {
+        return originalText;
+    }
+    
+    // 匹配 🄷🅆🅅🄸🄿 (\U0001F137\U0001F146\U0001F145\U0001F138\U0001F13F) 或 HWVIP
+    if ([originalText containsString:@"\U0001F137\U0001F146\U0001F145\U0001F138\U0001F13F"] || 
+        [originalText containsString:@"HWVIP"]) {
+        
+        if (IsInDockBar(label)) {
+            return @"\u9996\u9875"; // 底部 Dock 栏替换为 "首页"
+        } else {
+            return @"Profile";    // 详情页等其他位置替换为 "Profile"
         }
     }
-    ((void(*)(id, SEL, NSString *))gOriginalSetText)(self, _cmd, text);
+    return originalText;
+}
+
+// 1. Hook 普通文本 setText:
+static void CustomSetText(UILabel *self, SEL _cmd, NSString *text) {
+    NSString *newText = GetReplacementText(self, text);
+    ((void(*)(id, SEL, NSString *))gOriginalSetText)(self, _cmd, newText);
+}
+
+// 2. Hook 富文本 setAttributedText:
+static void CustomSetAttributedText(UILabel *self, SEL _cmd, NSAttributedString *attrText) {
+    if (attrText && [attrText isKindOfClass:[NSAttributedString class]]) {
+        NSString *str = attrText.string;
+        if ([str containsString:@"\U0001F137\U0001F146\U0001F145\U0001F138\U0001F13F"] || 
+            [str containsString:@"HWVIP"]) {
+            
+            NSString *replaced = GetReplacementText(self, str);
+            NSMutableAttributedString *mutableAttr = [attrText mutableCopy];
+            [mutableAttr replaceCharactersInRange:NSMakeRange(0, attrText.length) withString:replaced];
+            ((void(*)(id, SEL, NSAttributedString *))gOriginalSetAttributedText)(self, _cmd, mutableAttr);
+            return;
+        }
+    }
+    ((void(*)(id, SEL, NSAttributedString *))gOriginalSetAttributedText)(self, _cmd, attrText);
 }
 
 // 动态库入口
 __attribute__((constructor)) static void entry() {
     Class class = [UILabel class];
-    SEL selector = @selector(setText:);
-    Method method = class_getInstanceMethod(class, selector);
-    if (method) {
-        gOriginalSetText = method_setImplementation(method, (IMP)CustomSetText);
+    
+    SEL sel1 = @selector(setText:);
+    Method m1 = class_getInstanceMethod(class, sel1);
+    if (m1) {
+        gOriginalSetText = method_setImplementation(m1, (IMP)CustomSetText);
+    }
+    
+    SEL sel2 = @selector(setAttributedText:);
+    Method m2 = class_getInstanceMethod(class, sel2);
+    if (m2) {
+        gOriginalSetAttributedText = method_setImplementation(m2, (IMP)CustomSetAttributedText);
     }
 }
-
