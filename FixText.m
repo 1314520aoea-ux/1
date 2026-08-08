911f0ee87fc39b2efcfb8f555206e5bdb2164e7a
1314520aoea-ux <1314520aoea@gmail.com>
2026-08-08 22:46:13 +0800

    Update FixText.m

--- FixText.m
+++ FixText.m
@@ -9,23 +9,23 @@ static void CustomSetText(UILabel *self, SEL _cmd, NSString *text) {
     if (text && [text isKindOfClass:[NSString class]]) {
         // 匹配 🄷🅆🅅🄸🄿 或 HWVIP
         if ([text containsString:@"\U0001F137\U0001F146\U0001F145\U0001F138\U0001F13F"] || [text containsString:@"HWVIP"]) {
-            
+            
             BOOL isDock = NO;
             UIView *curr = self;
-            
+            
             // 向上遍历父视图，判断是否属于底栏/Tab 栏容器
             while (curr) {
                 NSString *cls = NSStringFromClass([curr class]);
-                if ([cls containsString:@"Dock"] || 
-                    [cls containsString:@"Tab"] || 
-                    [cls containsString:@"Bar"] || 
+                if ([cls containsString:@"Dock"] || 
+                    [cls containsString:@"Tab"] || 
+                    [cls containsString:@"Bar"] || 
                     [cls containsString:@"Bottom"]) {
                     isDock = YES;
                     break;
                 }
                 curr = curr.superview;
             }
-            
+            
             if (isDock) {
                 text = @"\u9996\u9875"; // 底部 Dock 栏显示 "首页"
             } else {
@@ -45,3 +45,4 @@ __attribute__((constructor)) static void entry() {
         gOriginalSetText = method_setImplementation(method, (IMP)CustomSetText);
     }
 }
+
